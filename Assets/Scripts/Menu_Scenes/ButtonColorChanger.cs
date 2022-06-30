90f751fecfc34b3a27ac2871eb897a5b1cbdfc44
Luis Keck <luis.keck@hs-furtwangen.de>
2022-06-30 17:23:44 +0200

    minor ui adjustments

--- Assets/Scripts/Menu_Scenes/ButtonColorChanger.cs
+++ Assets/Scripts/Menu_Scenes/ButtonColorChanger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 
@@ -10,29 +11,48 @@ public class ButtonColorChanger : MonoBehaviour
     public Color OnHover = new Color(1, 1, 1);
 
     private TextMeshProUGUI ButtonLabel;
+    private Image Image;
 
     private void OnEnable()
     {
-        if (ButtonLabel != null)
-            ButtonLabel.color = Default;
+        ToDefaultColor();
     }
+
     private void Start()
     {
-        ButtonLabel = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        if (transform.GetChild(0).GetComponent<TextMeshProUGUI>())
+            ButtonLabel = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        else if (transform.GetChild(0).GetComponent<Image>())
+            Image = transform.GetChild(0).GetComponent<Image>();
+
+    }
+    private void OnDestroy()
+    {
+        ToDefaultColor();
     }
 
     public void OnPointerEnter()
     {
-        ButtonLabel.color = OnHover;
+        if (ButtonLabel != null)
+            ButtonLabel.color = OnHover;
+        else if (Image != null)
+            Image.color = OnHover;
     }
 
     public void OnPointerExit()
     {
-        ButtonLabel.color = Default;
+        ToDefaultColor();
     }
 
     public void OnClick()
     {
-        ButtonLabel.color = Default;
+        ToDefaultColor();
+    }
+    void ToDefaultColor()
+    {
+        if (ButtonLabel != null)
+            ButtonLabel.color = Default;
+        else if (Image != null)
+            Image.color = Default;
     }
 }

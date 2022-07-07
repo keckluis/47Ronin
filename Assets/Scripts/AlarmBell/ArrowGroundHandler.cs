611d660a55a03c2684482b581394e3f4b2666118
Luis Keck <luis.keck@hs-furtwangen.de>
2022-07-07 17:28:10 +0200

    new grass ground for all levels

--- Assets/Scripts/AlarmBell/ArrowGroundHandler.cs
+++ Assets/Scripts/AlarmBell/ArrowGroundHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ArrowGroundHandler : MonoBehaviour
 {
+    public Transform GroundArrowParent;
+    public int MaxArrows = 25;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Arrow")
@@ -11,6 +13,15 @@ public class ArrowGroundHandler : MonoBehaviour
             collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             Destroy(collision.gameObject.GetComponent<Rigidbody2D>());
             Destroy(collision.gameObject.GetComponent<Collider2D>());
+            collision.transform.parent = GroundArrowParent;
+        }
+    }
+
+    private void Update()
+    {
+        if (GroundArrowParent.childCount > MaxArrows)
+        {
+            Destroy(GroundArrowParent.GetChild(0).gameObject);
         }
     }
 }

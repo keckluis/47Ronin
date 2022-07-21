f8cfb879f6466b93a30541657c3f0b15f07d2716
Lichtfarbenspiel <s.belua.d@gmail.com>
2022-07-21 16:05:00 +0200

    Player Hint added to mini game scenes

--- Assets/Scripts/Menu_Scenes/DisableCanvas.cs
+++ Assets/Scripts/Menu_Scenes/DisableCanvas.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class DisableCanvas : MonoBehaviour
 {
-    public int seconds = 15;
+    public int seconds = 10;
+    public bool top = false;
     GameObject Image;
     bool move = false;
+    Vector3 dir = Vector3.down;
+
     void Awake() 
     {
-        Image = GameObject.Find("HintImage");
-    }
+        Image = GameObject.Find("HintImage");        
+        if (top) {
+            RectTransform pos = Image.transform.GetComponent<RectTransform>();
+            pos.transform.position = new Vector3(pos.transform.position.x, 1025, pos.transform.position.z);
+            pos.transform.localRotation = Quaternion.Euler(new Vector3(180, 0, 0));
+            GameObject text = GameObject.Find("HintText (TMP)");
+            text.transform.localScale = new Vector3(1, -1, 1);
+            text.transform.localPosition = new Vector3(0, 100, 0);
+            dir = Vector3.up;
+        }
+}
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +33,9 @@ public class DisableCanvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (move)
-            Image.transform.Translate(Vector3.down * Time.deltaTime * 600, Space.World);
+        if (move){
+            Image.transform.Translate(dir * Time.deltaTime * 600, Space.World);
+        }    
     }
 
     IEnumerator Disable()
@@ -30,6 +43,6 @@ public class DisableCanvas : MonoBehaviour
         yield return new WaitForSeconds(seconds);
         move = true;    
         yield return new WaitForSeconds(5);
-        GameObject.Destroy(this);
+        this.enabled = false;
     }
 }

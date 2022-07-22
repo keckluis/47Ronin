37e970a22014acfc7777d93d2acfdc05d0d9b676
MarKs <markus.schuermann@hs-furtwangen.de>
2022-07-22 23:57:55 +0200

    Vase Audio added
    Unofficial Release Patch lol

--- Assets/Scripts/SmokeVase.cs
+++ Assets/Scripts/SmokeVase.cs
@@ -6,14 +6,22 @@ public class SmokeVase : MonoBehaviour
 {
     public GameObject SmokeCload;
     public GameObject DetectionCollider;
+    AudioSource smashSound;
+
+    private void Start()
+    {
+        smashSound = GameObject.Find("AudioVase").GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.layer == 8)
         {
+            smashSound.timeSamples = 6;
+            smashSound.Play();
             Destroy(DetectionCollider);
             Vector3 SmokeOffset = new Vector3(transform.position.x - 0.15f, transform.position.y + 1.239f, transform.position.z);
-            GameObject SmokeCloadInst = GameObject.Instantiate(SmokeCload, SmokeOffset, transform.rotation);
+            GameObject SmokeCloadInst = GameObject.Instantiate(SmokeCload, SmokeOffset, transform.rotation);           
             Destroy(gameObject);
             Destroy(collision.gameObject);
         }
